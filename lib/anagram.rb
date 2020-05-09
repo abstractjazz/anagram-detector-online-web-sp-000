9aa6d3f95380c718a58a249c2833d595e65e192e
Evan Burton <evan@heyevan.com>
2020-05-09 20:36:37 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,14 +8,13 @@ def initialize(word)
 end
 
 def match(word)
-if @word.split.sort == word.split.sort
+if word.split.sort == @word.split.sort
 word.split.sort
 # sort == word.sort
 #   #end
 # end
     end
   end
-  binding.pry
 end
 
 
