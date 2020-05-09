9b112b6a3b21921f5dcd098e8b8d608c67e13b42
Evan Burton <evan@heyevan.com>
2020-05-09 20:57:35 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,7 @@ def initialize(word)
 end
 
 def match(word_array)
-word_array.select {|word| word.split("") == @word("").split}
+word_array.select {|word| word.split("") == @word.split("")}
   end
 
 
